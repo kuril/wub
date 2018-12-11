ccafe5927b0da05c0f9c342162bd74cb2a710ab0
Kirill Romanychev <kuril@kuril.ru>
2018-12-11 08:36:52 +0100

    app with router

---
+++ src/navigation/NavBar.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import * as classnames from "classnames"
+import { Link } from "react-router-dom"
+
+export type NavBarLink = { title: string, path: string };
+export type NavBarOwnProps = { links: NavBarLink[] };
+type NavBarProps = NavBarOwnProps;
+type NavBarState = { menuActive: boolean };
+
+export class NavBar extends React.Component<NavBarProps, NavBarState> {
+    constructor(props: any) {
+        super(props);
+        this.state = { menuActive: false };
+    }
+    render() {
+        return (
+            <nav className="navbar" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="/#">
+                        <span className="icon">
+                            <i className="fas fa-home"></i>
+                        </span>
+                    </a>
+
+                    <a role="button" className="navbar-burger"
+                        aria-label="menu" aria-expanded="false"
+                        onClick={() => this.setState({ menuActive: !this.state.menuActive })}>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div className={classnames("navbar-menu", { ["is-active"]: this.state.menuActive })}>
+                    {this.props.links.map(
+                        ({title, path}) => (<Link to={path} className="navbar-item" key={path}>{title}</Link>)
+                    )}
+                </div>
+            </nav>
+        )
+    }
+}
---
+++ src/pages/Page2.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export class Page2 extends React.Component {
+    render() {
+        return (<div className="page-container">
+            <h1>PAGE 2</h1>
+            <section>
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </section>
+        </div>)
+    }
+}
